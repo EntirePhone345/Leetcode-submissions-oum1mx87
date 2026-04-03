ad0491a0b8248abd6d3bb11316072c96c3be367e
EntirePhone345 <sarveshpatil0104@gmail.com>
2026-04-04 00:38:55 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long fibo(int n){
+        if(n <= 1) return n;
+
+    int prev2 = 0, prev1 = 1;
+
+    for(int i = 2; i <= n; i++) {
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+
+    return prev1;
+    }
+    long long climbStairs(int n) {
+        return fibo(n+1);
+    }
+};
