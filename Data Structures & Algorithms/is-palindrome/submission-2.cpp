702c938600446682b9c23b8fa74ad4ab6cf36e3c
EntirePhone345 <sarveshpatil0104@gmail.com>
2026-07-13 14:52:37 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeNonAlphanumeric(string s) {
+        string result;
+        for (char c : s) {
+            if (isalnum((unsigned char)c)) {
+                result += tolower((unsigned char)c);
+            }
+        }
+        return result;
+    }
+    bool isPalindrome(string s) {
+        s = removeNonAlphanumeric(s);
+        int n = s.size()-1;
+       
+            for(int i = 0 ; i < s.size()/2 ; i++){
+                if(s[i]!=s[n-i]){
+                    return false;
+                }
+            }
+       return true;
+
+    }
+};
